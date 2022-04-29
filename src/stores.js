24f9626c3eda7be267e6bec354b3ce5c1fdb35c0
Frankie Mercado <oeluer@gmail.com>
2022-04-29 16:27:21 -0700

    use class directive and writable store

--- src/stores.js
+++ src/stores.js
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store";
 
-export const answers = writable([]);
+export const currentQuestion = writable(1);
 export const cells = writable(Array(3).fill(Array(3).fill(0)));
+export const answers = writable([]);
 export const theme = writable("neu-light");
